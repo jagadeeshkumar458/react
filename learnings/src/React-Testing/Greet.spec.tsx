477309a2503a336d3e8a43be3aad4bb28ad2d5d3
jagadeeshkumar458 <jagadeeshkumar44444@gmail.com>
2026-01-07 20:59:19 +0530

    Testing - File name conventions

---
+++ learnings/src/React-Testing/Greet.spec.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react"
+import Greet from "./Greet"
+
+describe('Greet', () => {
+    it('Renders Correctly', () => {
+        render(<Greet />);
+        const element = screen.getByText(/Hello/i);
+        expect(element).toBeInTheDocument();
+    })
+
+    it('renders with a name', () => {
+        render(<Greet name = 'Jagadeesh'/>);
+        const textElement = screen.getByText('Hello Jagadeesh');
+        expect(textElement).toBeInTheDocument();
+    })
+})
