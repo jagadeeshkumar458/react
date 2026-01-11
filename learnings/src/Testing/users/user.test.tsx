9b60cd26aa40f26fe2e72719592d82d993afee58
jagadeeshkumar458 <jagadeeshkumar44444@gmail.com>
2026-01-12 00:59:08 +0530

    Testing - MSW error handling

--- learnings/src/Testing/users/user.test.tsx
+++ learnings/src/Testing/users/user.test.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import Users from './user';
+import { server } from '../mocks/server';
+import { rest } from 'msw';
 
 describe('Users Component', () => {
     test('renders correctly', () => {
@@ -12,5 +14,20 @@ describe('Users Component', () => {
         render(<Users />);
         const users = await screen.findAllByRole('listitem');
         expect(users.length).toHaveLength(3);
+    });
+
+    test('renders error', async () => {
+        server.use(
+            rest.get('https://jsonplaceholder.typicode.com/users', (req, res, ctx) => {
+                return res(
+                    ctx.status(500),
+                    ctx.json({ message: 'Internal Server Error' })
+                );
+            })
+        );
+        
+        render(<Users />);
+        const errorMessage = await screen.findByText('error fetching users');
+        expect(errorMessage).toBeInTheDocument();
     });
 });
