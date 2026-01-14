b23070059c5d4bb63589f1a1c4908910fd334fde
jagadeeshkumar458 <jagadeeshkumar44444@gmail.com>
2026-01-14 23:57:52 +0530

    Router - URL Params

--- learnings/src/Router/UserDetails.tsx
+++ learnings/src/Router/UserDetails.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 export const UserDetails = () => {
+    const params = useParams();
+    const userId = params.userId
   return (
-    <div>UserDetails</div>
+    <div>UserDetails: {userId}</div>
   )
 }
