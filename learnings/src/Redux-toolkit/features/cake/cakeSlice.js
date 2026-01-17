4b9ad9cea85e2f338c59e1b9a24bdaabb65586dc
jagadeeshkumar458 <jagadeeshkumar44444@gmail.com>
2026-01-17 13:32:29 +0530

    Redux Toolkit - cake slice

--- learnings/src/Redux-toolkit/features/cake/cakeSlice.js
+++ learnings/src/Redux-toolkit/features/cake/cakeSlice.js
@@ -9,6 +9,8 @@ const cakeSlice = createSlice({
   initialState,
   reducers: {
     ordered: (state) => {
+      // WE CAN DIRECTLY MODIFY THE STATE HERE BECAUSE CREATE SLICE UNDER THE HOOD
+      // USES IMMER LIBRARY
       state.numOfCakes--;
     },
     restocked: (state, action) => {
@@ -16,6 +18,10 @@ const cakeSlice = createSlice({
     },
   },
 });
+
+// THIS SLICE EFFECTIVRLY TAKES CARE OF DEFINING AN ACTION TYPE CONSTANT
+// AN ACTION OBJECT AND ACTION CREATOR AND SWITCH STATEMENTS IN THE REDUCER
+// AND HANDLING IMMUTABLE UPDATES IN THE REDUCER
 
 export default cakeSlice.reducer;
 export const { ordered, restocked } = cakeSlice.actions;
