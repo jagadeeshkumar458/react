6a0dd0c4f3432ff41a97c3be22f0156a75e7977a
jagadeeshkumar458 <jagadeeshkumar44444@gmail.com>
2026-01-17 16:16:32 +0530

    Redux Toolkit - useSelector

--- learnings/src/Redux-toolkit/features/icecream/IcecreamView.tsx
+++ learnings/src/Redux-toolkit/features/icecream/IcecreamView.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 export const IcecreamView = () => {
+    const numOficecreams = useSelector((state: any) => state.icecream.numOfIcecreams)
   return (
     <div>
-        <h2>Number of icecreams - </h2>
+        <h2>Number of icecreams - {numOficecreams}</h2>
         <button>Order icecream</button>
         <button>Restock icecreams</button>
     </div>
