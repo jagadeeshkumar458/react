3a8fcfe8f095a789520ba9b7ebedfd0b8ee10b97
jagadeeshkumar458 <jagadeeshkumar44444@gmail.com>
2026-01-19 00:04:49 +0530

    Redux - Bind action creators

--- learnings/src/Redux-toolkit/ReduxDemo/index.js
+++ learnings/src/Redux-toolkit/ReduxDemo/index.js
@@ -1,6 +1,7 @@
 // import { configureStore } from "redux";
 const redux = require("redux");
 const createStore = redux.createStore;
+const bindActionCreators = redux.bindActionCreators;
 
 const CAKE_ORDERED = "CAKE_ORDERED";
 const CAKE_RESTOCKED = "CAKE_RESTOCKED";
@@ -47,9 +48,15 @@ const unsubscribe = store.subscribe(() =>
   console.log("updated state", store.getState())
 );
 
-store.dispatch(orderCake());
-store.dispatch(orderCake());
-store.dispatch(orderCake());
-store.dispatch(restockCake(3));
+// store.dispatch(orderCake());
+// store.dispatch(orderCake());
+// store.dispatch(orderCake());
+// store.dispatch(restockCake(3));
+
+const actions = bindActionCreators({ orderCake, restockCake }, store.dispatch);
+actions.orderCake();
+actions.orderCake();
+actions.orderCake();
+actions.restockCake(3);
 
 unsubscribe();
