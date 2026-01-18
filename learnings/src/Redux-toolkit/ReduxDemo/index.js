1200753bc6fed39d796eee1cc61563cfccb76629
jagadeeshkumar458 <jagadeeshkumar44444@gmail.com>
2026-01-19 00:01:26 +0530

    Redux - Restocking cakes

--- learnings/src/Redux-toolkit/ReduxDemo/index.js
+++ learnings/src/Redux-toolkit/ReduxDemo/index.js
@@ -3,11 +3,19 @@ const redux = require("redux");
 const createStore = redux.createStore;
 
 const CAKE_ORDERED = "CAKE_ORDERED";
+const CAKE_RESTOCKED = "CAKE_RESTOCKED";
 
 function orderCake() {
   return {
     type: "CAKE_ORDERED",
     quantity: 1,
+  };
+}
+
+function restockCake(qty = 1) {
+  return {
+    type: "CAKE_RESTOCKED",
+    payload: qty,
   };
 }
 
@@ -21,6 +29,11 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         numOfCakes: state.numOfCakes - 1,
+      };
+    case CAKE_RESTOCKED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes + action.payload,
       };
     default:
       return state;
@@ -37,5 +50,6 @@ const unsubscribe = store.subscribe(() =>
 store.dispatch(orderCake());
 store.dispatch(orderCake());
 store.dispatch(orderCake());
+store.dispatch(restockCake(3));
 
 unsubscribe();
