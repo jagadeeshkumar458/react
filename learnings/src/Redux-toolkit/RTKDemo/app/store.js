8e61c5c00ae9e5c6d36252000cf3df52ab6f0da3
jagadeeshkumar458 <jagadeeshkumar44444@gmail.com>
2026-01-19 23:44:03 +0530

    Redux Toolkit - Logger Middleware

--- learnings/src/Redux-toolkit/RTKDemo/app/store.js
+++ learnings/src/Redux-toolkit/RTKDemo/app/store.js
@@ -1,12 +1,18 @@
 const configureStore = require("@reduxjs/toolkit").configureStore;
 const cakeReducer = require("../features/cake/cakeSlice");
 const icecreamReducer = require("../features/icecream/icecreamSlice");
+const reduxLogger = require("redux-logger");
+
+const logger = reduxLogger.createLogger();
 
 const store = configureStore({
   reducer: {
     cake: cakeReducer,
     icecream: icecreamReducer,
   },
+  // BY DEFAULT CONFIGURE STORE FUNCTION ADDS SOME MIDDLEWARE TO THE REDUX STORE SETUP
+  // AUTOMATICALLY, SO CONCAT THE LOGGER TO THE DEFAULT LIST
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 // WITH REDUX TOOLKIT THE CONFIGURE STORE WILL HANDLE THE CONBINE REDUCERS
 // UNDER THE HOOD
