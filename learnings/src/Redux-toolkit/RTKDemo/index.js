f8d3074d0d081db0b93e71d4000a32376e16f1f9
jagadeeshkumar458 <jagadeeshkumar44444@gmail.com>
2026-01-19 23:30:09 +0530

    Redux Toolkit - Configuring Store

---
+++ learnings/src/Redux-toolkit/RTKDemo/index.js
@@ -0,0 +1,13 @@
+const store = require("../RTKDemo/app/store");
+const cakeActions = require("../RTKDemo/features/cake/cakeSlice").cakeActions;
+
+console.log("Initial state", store.getState());
+const unsubscribe = store.subscribe(() => {
+  console.log("Updated state", store.getState());
+});
+
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.restocked(3));
+unsubscribe();
