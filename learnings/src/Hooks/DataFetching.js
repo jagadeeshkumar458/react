a2a0efca8b3674f92e17ab8af22e4b530c3204c0
jagadeeshkumar458 <jagadeeshkumar44444@gmail.com>
2026-01-24 23:34:29 +0530

    Hooks - fetch individual data using useEffect

--- learnings/src/Hooks/DataFetching.js
+++ learnings/src/Hooks/DataFetching.js
@@ -2,23 +2,34 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 function DataFetching() {
-  const [posts, setPosts] = useState([]);
+  const [post, setPost] = useState([]);
+  const [id, setId] = useState(1);
+  const [idFromButtonClick, setIdFromButtonClick] = useState(1);
+
+  const handleClick = () => {
+    setIdFromButtonClick(id);
+  };
 
   useEffect(() => {
     axios
-      .get("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => setPosts(response.data))
+      .get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
+      .then((response) => setPost(response.data))
       .catch((err) => {
         console.log(err);
       });
-  }, []);
+  }, [idFromButtonClick]);
   return (
     <div>
-      <ul>
+      <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+      <button type="button" onClick={handleClick}>
+        Fetch Post
+      </button>
+      {/* <ul>
         {posts.map((item) => (
           <li key={item.id}>{item.title}</li>
         ))}
-      </ul>
+      </ul> */}
+      <div>{post.title}</div>
     </div>
   );
 }
